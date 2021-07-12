ec048d60d71a9acec4c0c7c184bb81e1317a379c
pete-afh <petehanner+2@gmail.com>
2021-07-12 19:03:29 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :moods
-  resources :users
+  post 'sessions/create'
+  post 'sessions/destroy'
+  resources :moods, only: [:create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create]
+  get "/login" => "users#index"
+  get "/signup" => "users#new"
 end
